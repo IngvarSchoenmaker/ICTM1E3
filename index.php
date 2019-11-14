38e89da9817a31e67de5881f86a0968564f685ab
IngvarSchoenmaker <ingvar@brownfox.nl>
2019-11-14 13:26:10 +0100

    search met surgesties

--- index.php
+++ index.php
@@ -3,32 +3,79 @@ require_once "db.php";
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title></title>
-    <link rel="stylesheet" type="text/css" href="db.php">
+    <meta charset="UTF-8">
+    <title>PHP Live MySQL Database Search</title>
+
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+    <style type="text/css">
+        body{
+            font-family: Arail, sans-serif;
+        }
+        /* Formatting search box */
+        .search-box{
+            width: 300px;
+            position: relative;
+            display: inline-block;
+            font-size: 14px;
+        }
+        .result{
+            position: absolute;
+            z-index: 999;
+            top: 100%;
+            left: 0;
+        }
+        .search-box input[type="text"], .result{
+            width: 100%;
+            box-sizing: border-box;
+        }
+        /* Formatting result items */
+        .result p{
+            margin: 0;
+            padding: 7px 10px;
+            border: 1px solid #CCCCCC;
+            border-top: none;
+            cursor: pointer;
+        }
+        .result p:hover{
+            background: #f2f2f2;
+        }
+    </style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('.search-box input[type="text"]').on("keyup input", function(){
+                /* Get input value on change */
+                let inputVal = $(this).val();
+                let resultDropdown = $(this).siblings(".result");
+                if(inputVal.length){
+                    $.get("surgestions.php", {term: inputVal}).done(function(data){
+                        // Display the returned data in browser
+                        resultDropdown.html(data);
+                    });
+                } else{
+                    resultDropdown.empty();
+                }
+            });
+
+            // Set search input value on click of result item
+            $(document).on("click", ".result p", function(){
+                $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+                $(this).parent(".result").empty();
+            });
+        });
+    </script>
 </head>
 <body>
- <form action="search.php" method="POST">
-     <input type="text" name="search" placeholder="Search">
-     <button type="submit" name="submit-search">Search</button>
- </form>
-<h1>Front page</h1>
-<h2>All articles:</h2>
-<div class="article-container">
-    <?php
-        $sql = "SELECT * FROM stockitems";
-        $result = mysqli_query($conn, $sql);
-        $queryResults = mysqli_num_rows($result);
-    if($queryResults > 0){
-        while ($row = mysqli_fetch_assoc($result)){
-            echo "<div>
-                <h3>".$row['StockItemName']."</h3>
-                <p>".$row['MarketingComments']."</p>
-            </div>";
-        }
-    }
-    ?>
-</div>
+<form action="search.php" method="POST">
+    <div class="search-box">
+        <input type="text" autocomplete="off" name="search" placeholder="Search" />
+        <button type="submit" name="submit-search">Search</button>
+        <div class="result"></div>
+    </div>
+</form>
 </body>
 </html>
