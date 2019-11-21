0ec450633e73759a467fcd0fea949425ebad9e5c
Pjoescall <p.boertien@student.alfa-college.nl>
2019-11-21 10:59:11 +0100

    hij pakt nu alles in de db

--- Pages/all_products.php
+++ Pages/all_products.php
@@ -1,3 +1,18 @@
 <?php
-include ''
+include '../incl/header.php';
+
+$sql = "SELECT * FROM stockitems ";
+$result = mysqli_query($conn, $sql);
+$queryResult = mysqli_num_rows($result);
+if($queryResult > 0){
+    ?>
+<div class="container" style="margin-top:150px; margin-bottom: 50px; text-align: left">
+    <?php
+    while ($row = mysqli_fetch_assoc($result)){
+        echo "<p>".$row['StockItemName']."</p>";
+    }
+}
 ?>
+</div>
+<?php
+include '../incl/footer.php';
