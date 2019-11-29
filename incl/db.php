c87de48ae9f9c5fd1bd394209031176a45d86f72
Pjoescall <p.boertien@student.alfa-college.nl>
2019-11-29 15:05:18 +0100

    beide db staan nu nu in db.php
    heb beide databases erin gegooid voor gemak dat je maar 1 file aanrpoet

--- incl/db.php
+++ incl/db.php
@@ -4,4 +4,12 @@ $username = "root";
 $password = "";
 $dbname = "onzedbwwi";
 $conn = mysqli_connect($server, $username, $password, $dbname);
+
+$servername = "localhost";
+$DBusername = "root";
+$DBpassword = "";
+$DBname = "wideworldimporters";
+$port = "3306";
+$conn2 = mysqli_connect($servername, $DBusername, $DBpassword, "wideworldimporters", $port) or
+die("Could not connect: " . mysqli_error());
 ?>
