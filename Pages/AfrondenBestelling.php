13b25654918957f527d8fea71f225ac3d0a0fd8c
Geertjan64 <geertjanverkuil@gmail.com>
2019-12-05 16:18:10 +0100

    Add config and confirm order

---
+++ Pages/AfrondenBestelling.php
@@ -0,0 +1,38 @@
+<?php
+//include '../Pages/ShoppingCartQueries.php';
+include '../incl/ConnectieFunctie.php';
+include '../incl/header.php';
+//session_start();
+print("\n");
+print("\n");
+print("\n");
+print("\n");
+print("\n");
+print("\n");
+print("Hallo");
+
+
+$_SESSION['cart'] = array(220 => 3, 221 => 4);
+$productList = $_SESSION['cart'];
+$_SESSION['ID'] = 2;
+$Customer_ID = $_SESSION['ID'];
+
+if(isset($_SESSION['ID'])) {
+    //Zet de bestelling in de database.
+    $OrderID = 0;
+    $Customer_ID = $_SESSION['ID'];
+    $Datum = date("Y/m/d");
+    $PaymentID = 2;
+
+    $statement = mysqli_prepare($conn = get_connection(), 'INSERT INTO Orders VALUES(?,?,?,?)');
+    mysqli_stmt_bind_param($statement, 'iisi', $OrderID, $Customer_ID, $Datum, $PaymentID);
+    mysqli_stmt_execute($statement);
+
+    //Haalt de shoppingcart weer leeg
+    $statement = mysqli_prepare($conn = get_connection(), 'DELETE FROM Shoppinglist WHERE Customer_ID =? ');
+    mysqli_stmt_bind_param($statement, 'i', $Customer_ID);
+    mysqli_stmt_execute($statement);
+
+}
+
+
--- incl/Database.php
+++ incl/Database.php
@@ -1,48 +1,8 @@
 <?php
+    include '../incl/ConnectieFunctie.php';
     session_start();
     $_CustomerID = $_SESSION['ID'];
-
-
-    //functie die connectie legt met de database.
-    //gemaakt zodat je connectie kan leggen in een andere functie.
-    function get_connection(){
-        $_database["server"] = "localhost";
-        $_database["username"] = "root";
-        $_database["password"] = "";
-        $_database["name"] = "onzedbwwi";
-        $_database["poort"] = "3306";
-
-        $conn = mysqli_connect($_database["server"], $_database["username"], $_database["password"], $_database["name"], $_database["poort"]);
-        if ($conn->connect_error) {
-            return die("Connection failed: " . $conn->connect_error);
-        }else {
-            return $conn;
-        }
-    }
-
-    function GetData($sql, $onlyOneRecord = false)
-    {
-        //Verbinding maken met de database
-        //Query uitvoeren
-        $conn = get_connection();
-        $statement = mysqli_prepare($conn, $sql);
-        mysqli_stmt_execute($statement);
-        $result = mysqli_stmt_get_result($statement);
-        //Zet de results in een array
-        $resultList = [];
-        while ($row = mysqli_fetch_assoc($result)) {
-            $resultList[] = $row;
-        }
-        $conn->close();
-        //Als de parameter van onlyonerecord op true staat
-        //wordt er slechts 1 item terug gegeven.
-        if ($onlyOneRecord) {
-            return $resultList[0];
-        }
-        //anders
-        return $resultList;
-    }
-
+    print($_CustomerID);
 
     // ################################################
     // Ophalen klant en adresgegevens
