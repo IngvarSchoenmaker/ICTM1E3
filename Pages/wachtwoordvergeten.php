9e64964c7cbacc49d4cd52662484e7fc3b8d8743
PascalBoertien <57621478+PascalBoertien@users.noreply.github.com>
2019-12-05 15:18:35 +0100

    bijna klaar met mail

--- Pages/wachtwoordvergeten.php
+++ Pages/wachtwoordvergeten.php
@@ -5,18 +5,15 @@
 <div class="container" style="margin-top:200px; margin-bottom: 233px; text-align: center">
     <div class="row">
         <div class="col-lg-12">
-            <form>
+            <form method="post" action="wachtwoordvergeten.php">
                 Emailadres:<br><input type="email" name="emailadres"><br><br>
-                <button type="submit" name="send">verstuur mail</button>
+                <button type="Submit" name="send">verstuur mail</button>
             </form>
-        </div>
-    </div>
 <?php
 if (isset($_POST['send'])) {
     $email = $_POST['emailadres'];
     if (empty($email)) { // hier wordt gecontroleerd of de input velden zijn ingevuld
         print("<p style=\" color:red; margin-right: 5px; margin-left: 400px\">Het verplichte veld is niet ingevuld</p>");
-        echo "ik niet doen XD";
     }
     else {
         $stmt = $conn->prepare("SELECT * FROM customer WHERE Email=?");
@@ -27,14 +24,17 @@ if (isset($_POST['send'])) {
         if ($row > 0){
             $to = $email;
             $subject = "wachtwoord veranderen";
-            $txt = "http://localhost/HBO/ICTM1E3/Pages/veranderww";
-            $headers = "From: pboertien@outllok.com";
+            $txt = "http://localhost/HBO/ICTM1E3/Pages/veranderww <br>
+                    ik verstuur een mail uwu";
+            $headers = "From: pboertien@outlook.com";
 
             mail($to,$subject,$txt,$headers);
         }
     }
 }
 ?>
+        </div>
+    </div>
 </div>
 <?php
     include '../incl/footer.php';
