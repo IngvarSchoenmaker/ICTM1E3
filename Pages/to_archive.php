5b056aa9f57f0d5ec0943fa33766f4a275b6ba06
PascalBoertien <57621478+PascalBoertien@users.noreply.github.com>
2019-12-11 13:41:41 +0100

    zet spul in het archive

---
+++ Pages/to_archive.php
@@ -0,0 +1,21 @@
+<?php
+include '../incl/db.php';
+?>
+<?php
+$sql = "SELECT * FROM customer where Validation = 0";
+$result = mysqli_query($conn, $sql);
+foreach($result as $t) {
+    if($t['Verloop'] === date('d/m/Y')){
+        $id = $t['Customer_ID'];
+        $sqlin = "INSERT INTO customer_archive SELECT * FROM customer WHERE Customer_ID = $id";
+        mysqli_query($conn, $sqlin);
+        $sqldel = "DELETE FROM customer WHERE Customer_ID = $id";
+        mysqli_query($conn, $sqldel);
+        echo "U account is verlopen";
+    } elseif($t['Verloop'] === "10/12/2019"){
+        //bruh
+    } else {
+        echo "Vergeet niet u account to valideren";
+    }
+}
+?>
