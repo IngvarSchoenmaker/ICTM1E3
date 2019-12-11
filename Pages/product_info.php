f653862b62fb1584a76905a6b35fbb08630a9570
PascalBoertien <57621478+PascalBoertien@users.noreply.github.com>
2019-12-11 15:29:45 +0100

    meeeeh

--- Pages/product_info.php
+++ Pages/product_info.php
@@ -1,45 +1,9 @@
 <?php
 include '../incl/header.php';
 ?>
-<body>
-
-<!-- beetje style om de text en images op de goeie plek te zetten -->
-<style>
-    .img {
-        height: 600px;
-        width: auto;
-        float:left;
-        border: 1px solid #021a40;
-    }
-
-    .text {
-        padding: 0px 10px;
-    }
-
-    .featured {
-        font-size: 100px;
-        padding: 0px 300px;
-    }
-
-    .starimage {
-        height: 20px;
-        width: auto;
-    }
-</style>
-<div class="container" style="margin-top:200px; margin-bottom:50px; text-align: center">
-    <div class="row">
-        <div class="col-lg-12">
-<!-- de image -->
-<div>
-    <image src="../recources/usb.png" class="img"></image>
-</div>
-
 <?php
-
 $productid = $_GET['item'];
-
 ?>
-
 
 <?php
 // database connection
@@ -50,15 +14,43 @@ $dbname = "wideworldimporters";
 $conn1 = mysqli_connect($server, $username, $password, $dbname);
 $sql1 = "SELECT * FROM stockitems WHERE StockItemID = $productid";
 $result = mysqli_query($conn1, $sql1);
-?>
+while ($row = mysqli_fetch_assoc($result)) {
+    $itemname = $row['StockItemName'];
+    ?>
+<body>
+<!-- beetje style om de text en images op de goeie plek te zetten -->
+<style>
+    .img {
+        height: 600px;
+        width: auto;
+        float:left;
+        border: 1px solid #021a40;
+    }
+    .text {
+        padding: 0px 10px;
+    }
+    .featured {
+        font-size: 100px;
+        padding: 0px 300px;
+    }
+    .starimage {
+        height: 20px;
+        width: auto;
+    }
+</style>
+<div class="container" style="margin-top:200px; margin-bottom:50px; text-align: center">
+    <div class="row">
+        <div class="col-lg-12">
+
+<!-- de image -->
+<div>
+    <img src="../recources/voorbeeld fotos/<?php echo "$itemname" ?>1.jpg" class="img">
+</div>
 
 <!-- Data printen op site -->
 <div class="text">
     <?php
-    while ($row = mysqli_fetch_assoc($result)) {
         echo "<p>". $row['StockItemName'] ."<br> €" . $row['RecommendedRetailPrice'] . "<br>" .  $row['MarketingComments'] . "<br>" . $row['TypicalWeightPerUnit']. " KG <br>" . $row['Size']."</p>";
-
-    $itemname = $row['StockItemName'];
     }
     ?>
 </div>
@@ -86,11 +78,9 @@ function addToCart ($productid, $amount) {
 </form>
 
 <?php
-
 if(isset($_POST['Amount'])) {
     $amount = $_POST['Amount'];
 }
-
 
 if(isset($_POST['addToCart'])) {
 
@@ -114,9 +104,7 @@ if(isset($_POST['addToCart'])) {
             $result1 = mysqli_query($conn2, $sql1);
 
             while ($row = mysqli_fetch_assoc($result1)) {
-
                 $stars = $row['Stars'];
-
             }
 
             $i = 0;
@@ -130,11 +118,9 @@ if(isset($_POST['addToCart'])) {
             <div>
                 Dit product heeft een rating van <?php echo $stars . "/5" ?> ontvangen!
             </div>
-
             <div>
                 <?php include 'Reviews.php' ?>
             </div>
-
 </div>
     </div>
 </div>
