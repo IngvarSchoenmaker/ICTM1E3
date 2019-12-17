421e7fc94dee76a145310af775cf4792c59312cb
Geertjan64 <geertjanverkuil@gmail.com>
2019-12-18 00:21:32 +0100

    Reviews definitief klaar.

--- Pages/Reviews.php
+++ Pages/Reviews.php
@@ -67,26 +67,27 @@ ob_start();
 //Als er op de knop plaatsreview is gedrukt
 //Worden de input velden opgeslagen in variablen.
 if (isset($_POST['plaatsreview'])) {
-    $productDB = $_SESSION['ProductID'];
+    if (!isset($_SESSION['ID'])) {
+        print "<script>alert('Je moet ingelogd zijn om een review te plaatsen!'); </script>";
+    } else {
+        $productDB = $_SESSION['ProductID'];
 //
 //    $statement = mysqli_prepare($conn = get_connection(), "INSERT INTO product_information VALUES($productDB, NULL)");
 //    mysqli_stmt_execute($statement);
 
-    $emailDB = $_POST['mail'];
-    $scoreDB = $_POST['star'];
-    $beoordelingDB = $_POST['beoordeling'];
-    //Prepared statement
-    $statement = mysqli_prepare($conn = get_connection(), "INSERT INTO reviews VALUES(?,?,?,?)");
+        $emailDB = $_POST['mail'];
+        $scoreDB = $_POST['star'];
+        $beoordelingDB = $_POST['beoordeling'];
+        //Prepared statement
+        $statement = mysqli_prepare($conn = get_connection(), "INSERT INTO reviews VALUES(?,?,?,?)");
 
-    mysqli_stmt_bind_param($statement, 'isis', $productDB, $emailDB, $scoreDB, $beoordelingDB);
-    mysqli_stmt_execute($statement);
-    $result = mysqli_stmt_get_result($statement);
+        mysqli_stmt_bind_param($statement, 'isis', $productDB, $emailDB, $scoreDB, $beoordelingDB);
+        mysqli_stmt_execute($statement);
+        $result = mysqli_stmt_get_result($statement);
 
-
-    if (isset($_SESSION['ID'])) {
         print "<script>alert('Je review is succesvol geplaatst!'); </script>";
+        header("Refresh:0");
     }
-    header("Refresh:0");
 }
 
 ob_end_flush();
