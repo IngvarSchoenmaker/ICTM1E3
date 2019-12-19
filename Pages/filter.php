bb581443caa33e5248bb290f18ca88abe16b160b
PascalBoertien <57621478+PascalBoertien@users.noreply.github.com>
2019-12-19 23:11:31 +0100

    schoon maken

--- Pages/filter.php
+++ Pages/filter.php
@@ -27,84 +27,59 @@ include "../incl/dbwwi.php";
 
 if (isset($_POST['huismerk'])) {
     $huismerk = $_POST['huismerk'];
-
     if($huismerk == "ans1") {
+        $sql = "SELECT * FROM stockitems WHERE brand LIKE ''";
+        $result = mysqli_query($conn, $sql);
 
-$sql = "SELECT * FROM stockitems WHERE brand LIKE ''";
-$result = mysqli_query($conn, $sql);
-
-while($row = mysqli_fetch_assoc($result)){
-    echo $row['StockItemName'] . "<br>" . $row['RecommendedRetailPrice'] . "<br><br>";
-}
-
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['StockItemName'] . "<br>" . $row['RecommendedRetailPrice'] . "<br><br>";
+        }
     } else {
         echo "Er zijn geen zoekresultaten gevonden met het gekozen filter";
     }
 }
 
 if(isset($_POST['sale'])) {
-
     $huismerk = $_POST['sale'];
-
     if($huismerk == "ans2") {
-
         $sql = "SELECT * FROM stockitems WHERE StockItemID IN (SELECT StockItemID FROM specialdeals)";
         $result = mysqli_query($conn, $sql);
 
-        while($row = mysqli_fetch_assoc($result)){
+        while ($row = mysqli_fetch_assoc($result)) {
             echo $row['StockItemName'] . "<br>" . $row['RecommendedRetailPrice'] . "<br><br>";
         }
-
     } else {
         echo "Er zijn geen zoekresultaten gevonden met het gekozen filter";
     }
-
-
 }
 
 if(isset($_POST['highlow'])) {
-
     $huismerk = $_POST['highlow'];
-
     if($huismerk == "ans3") {
-
         $sql = "SELECT * FROM stockitems ORDER BY RecommendedRetailPrice DESC";
         $result = mysqli_query($conn, $sql);
 
         while($row = mysqli_fetch_assoc($result)){
             echo $row['StockItemName'] . "<br>" . $row['RecommendedRetailPrice'] . "<br><br>";
         }
-
     } else {
         echo "Er zijn geen zoekresultaten gevonden met het gekozen filter";
     }
-
-
 }
 
 if(isset($_POST['lowhigh'])) {
-
     $huismerk = $_POST['lowhigh'];
-
     if($huismerk == "ans4") {
-
         $sql = "SELECT * FROM stockitems ORDER BY RecommendedRetailPrice ASC";
         $result = mysqli_query($conn, $sql);
 
         while($row = mysqli_fetch_assoc($result)){
             echo $row['StockItemName'] . "<br>" . $row['RecommendedRetailPrice'] . "<br><br>";
         }
-
     } else {
         echo "Er zijn geen zoekresultaten gevonden met het gekozen filter";
     }
-
-
 }
-?>
-
-
-
-
+?>  
 </body>
 </html>
